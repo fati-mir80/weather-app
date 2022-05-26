18b2dd1a57b89c1810d185f1fec6dd7524800e0d
fati <100193726+fati-mir80@users.noreply.github.com>
2022-05-26 20:06:01 +0430

    removing

--- js.js
+++ js.js
@@ -130,7 +130,7 @@ function displayCelsiusTemperature(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-let celsiusTemperature = null;
+
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
